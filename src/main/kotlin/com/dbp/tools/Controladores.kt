264031b9fa5d365b104d339e1897117119e28865
David Blanco Paris <dblanco@varma.com>
2021-11-18 22:13:52 +0100

    Primer controlador

---
+++ src/main/kotlin/com/dbp/tools/Controladores.kt
@@ -0,0 +1,12 @@
+package com.dbp.tools
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/core")
+class CoreController{
+    @GetMapping("/version")
+    fun getVersion()="1.0.0";
+}
