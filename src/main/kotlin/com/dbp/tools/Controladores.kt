bd7e688d001bf791e3260a1b10bd8cf0de4aae3f
David Blanco Paris <dblanco@varma.com>
2021-11-20 22:40:11 +0100

    Correciones de avisos de código

--- src/main/kotlin/com/dbp/tools/Controladores.kt
+++ src/main/kotlin/com/dbp/tools/Controladores.kt
@@ -1,11 +1,10 @@
 package com.dbp.tools
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.security.oauth2.core.user.OAuth2User
+
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.util.*
+
 
 
 @RestController
